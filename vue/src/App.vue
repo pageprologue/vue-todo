14bc1498970c4a53afb739ebc37b7f67f73d2772
Chloe <page.prologue@gmail.com>
2020-08-10 17:41:29 +0900

    vue cli init

---
+++ vue/src/App.vue
@@ -0,0 +1,45 @@
+<template>
+  <div id="app">
+    <h2> {{ msg }} </h2>
+    <div class="wrap">
+      <div class="left-container">
+        <p>contents</p>
+      </div>
+      <div class="right-container">
+        <img src="../../image/lifecycle.png">
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
+
+<style>
+.wrap {
+  width: 100%;
+  display: flex;
+}
+h2 {
+  text-align: center;
+}
+.left-container {
+  width: 50%;
+  text-align: center;
+}
+.right-container {
+  width: 50%;
+  text-align: center;
+}
+img {
+  width: 30%;
+}
+</style>
